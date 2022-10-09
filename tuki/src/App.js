e8fb0beb52ce141fc1d3e155ba69996d6c2dac35
pablotroncosog <pablo.troncoso@mail.udp.cl>
2022-10-09 15:44:07 +0000

    primer commit

--- tuki/src/App.js
+++ tuki/src/App.js
@@ -4,52 +4,71 @@ import React, { useState, useEffect } from "react";
 
 function App() {
 
-  const [newItem, setnewItem] = useState("");
-  const [items, setItems] = useState([]);
+  const [actividad, setActividad] = useState("");
+  const [actividades, setActividades] = useState([]);
 
-
-  useEffect(()=> {
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/pablotr', {
-      method: "GET",
-         /* body: JSON.stringify([]), */
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp, "respuesta"); 
-        return resp.json();
-    })
-    .then(data => {
-        console.log(data); 
-    })
-    .catch(error => {
-        console.log(error);
-    });
-  }, [] )
-
+  useEffect(() => {
+    getTareas()
+  }, []) 
 
   
-  function addItem() {
-
-    if (!newItem) {
-      alert("Escribre tu actividad")
-      return;
-    }
-    const item = {
-      id: Math.floor(Math.random() * 1000),
-      value: newItem
-    }
-    setItems(oldList => [...oldList, item])
-    setnewItem("");
+  const getTareas = () => {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/pablotroncoso', 
+    {
+      method: "GET",
+     
+    })
+    .then(resp => {
+      console.log(resp, "respuesta");
+      return resp.json();
+    })
+    .then(data => {setActividades(data);})
+    .catch(error => {
+      console.log(error);
+    });
   }
 
-  function deleteItem(id) {
-
-    const newArray = items.filter(item => item.id !== id);
-    setItems(newArray)
+  const putTareas = (actividad) => {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/pablotroncoso', 
+    {
+      method: "PUT",
+      Headers: {"Content-Type": "application/json"},
+      body:JSON.stringify(actividad)
+     
+    })
+    .then(resp => {
+      console.log(resp, "respuesta");
+      return resp.json();
+    })
+    .then(data => {console.log(data);})
+    .catch(error => {
+      console.log(error);
+    });
   }
 
+
+  /* Guardar */
+  const nuevaActividad = (event) => {
+    setActividad(event.target.value);
+
+  }
+
+  /* Borrar */
+
+  const borrarActividad = (indice) => {
+    setActividades(actividades.filter((actividad, index) => index !== indice))
+
+  }
+
+  const guardarActividad = (event) => {
+    event.preventDefault();
+    let nuevaAct =  {label: actividad, done: false}
+    setActividades(actividades.concat(nuevaAct))
+    setActividad("")
+    /* Se aplica el metodo PUT del Fetch para guardar la tarea */
+    let newTasks = actividades.concat(nuevaAct)
+    putTareas(newTasks)
+  }
 
   return (
 
@@ -57,30 +76,40 @@ function App() {
       <div className="row align-items-center">
         <div className='row'>
 
-          <h2 className='mb-2'>Escribe tu actividad</h2>
-          <form onSubmit={ev => {
-            ev.preventDefault();
-            setnewItem(ev.target.texteo.value)
-          }}>
+          <h2 className='mb-2'>Escribe tu actividad: </h2>
+          <form onSubmit={guardarActividad}>
 
             <input
               type="text"
               placeholder="Escribe pls..."
-              value={newItem}
               className="form-control mb-4"
-              onChange={e => setnewItem(e.target.value)}
+              value={actividad.label}
+              onChange={nuevaActividad} /* EVENTO PARA CAPTURAR LO QUE SE ESCRIBE EN EL IMPUT EN NUESTRO ESTADO DEL NUEVO ITEM*/
             />
-
-            <button className='btn btn-primary' id='botonsito' onClick={() => addItem()}>Add</button>
-
-            <ul>
-              {items.map(item => { return <li key={item.id}> {item.value}  <button className='btn btn-primary' id='deletebtn' onClick={() => deleteItem(item.id)}> x</button> </li> })}
-            </ul>
-
           </form>
+
+          <div className='row mt-5'>
+            <div className='col-md-12'>
+              {actividades.length > 0 ?
+                actividades.map((actividad, indice) => (
+                  <div className='card'
+                    key={actividad.label}
+                    style={{ cursor: "pointer" }}
+                    onClick={() => borrarActividad(indice)}
+                  >
+                    <div className='card-body'> {actividad.label}
+                    </div>
+                  </div>
+                )) : "No hay actividades pendientes"}
+
+            </div>
+          </div>
+
+
+
         </div>
       </div>
-    </div>
+    </div >
 
   );
 }
